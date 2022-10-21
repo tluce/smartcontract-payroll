e12feb4e148a25f2d9ac754c06b2b01534b1913a
TL <34001240+tluce@users.noreply.github.com>
2022-10-21 11:53:00 +0200

    added hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,52 @@ require("hardhat-contract-sizer");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://goerli.infura.io/v3/";
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL || "https://mainnet.infura.io/v3/";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const REPORT_GAS = process.env.REPORT_GAS || false;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 5,
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 1,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: REPORT_GAS,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
   solidity: "0.8.17",
 };
